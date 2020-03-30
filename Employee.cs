59409dbb8408b9246d38438d3ef5c52aec059998
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-03-30 13:38:51 +0200

    Initial commit

---
+++ Employee.cs
@@ -0,0 +1,40 @@
+﻿namespace EmployeesV10
+{
+    public class Employee
+    {
+        #region Instance fields
+        private string _name;
+        private int _hoursPerWeek;
+        #endregion
+
+        #region Constructor
+        public Employee(string name, int hoursPerWeek)
+        {
+            _name = name;
+            _hoursPerWeek = hoursPerWeek;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int HoursPerWeek
+        {
+            get { return _hoursPerWeek; }
+            set { _hoursPerWeek = value; }
+        }
+
+        public virtual string AllInformation
+        {
+            get
+            {
+                return $"Employee {Name} works {HoursPerWeek} hours/week";
+            }
+        } 
+        #endregion
+    }
+}
